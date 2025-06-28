4b5fae954efa3280c4cde62676a1fea387c42ac5
isabella-mguess <isabellamguess@gmail.com>
2025-06-28 06:19:27 -0500

    Added Features

--- newfile.py
+++ newfile.py
@@ -1,3 +1,6 @@
 print("=================================")
 print("This is a test file for Python 3.11.4")
 print("=================================")
+print("This is a new file in the git-practice directory.")
+print("I hope this works...")
+print("=================================")
